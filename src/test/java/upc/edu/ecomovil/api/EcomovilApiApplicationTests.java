8f7c9c03c43b4283ac618d3520b36d5735190f93
Juan Calisaya <u202121935@upc.edu.pe>
2024-11-17 18:52:17 -0500

    feat:add api

---
+++ src/test/java/upc/edu/ecomovil/api/EcomovilApiApplicationTests.java
@@ -0,0 +1,12 @@
+package upc.edu.ecomovil.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomovilApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
